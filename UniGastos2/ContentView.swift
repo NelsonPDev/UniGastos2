6cb3d56be62925c0c085fa719ed954f6ca96654a
Citlaly <citla.balca2@gmail.com>
2026-03-25 15:07:38 -0600

    Initial Commit

---
+++ UniGastos2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UniGastos2
+//
+//  Created by Alumno on 25/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UniGastos2/UniGastos2App.swift
@@ -0,0 +1,17 @@
+//
+//  UniGastos2App.swift
+//  UniGastos2
+//
+//  Created by Alumno on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct UniGastos2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
